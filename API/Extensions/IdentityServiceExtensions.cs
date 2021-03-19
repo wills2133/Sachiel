85f2b020906d76bc976c0919843d2094d74336d9
wills2133 <wills2133@hotmail.com>
2021-03-19 14:06:50 -0700

    update: 12.131 add user entity, Identity framework, seed users

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,25 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityService(this IServiceCollection service,
+            IConfiguration config)
+        {
+            service.AddIdentityCore<AppUser>(opt => {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<DataContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            service.AddAuthentication();
+
+            return service;
+        }
+    }
+}
