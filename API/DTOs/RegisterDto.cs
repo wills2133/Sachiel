9a88d49672316a8052a2e44a2b3f2fae6d86a7a4
wills2133 <wills2133@hotmail.com>
2021-03-19 23:18:50 -0700

    update: 12.139 (1) AccountController add register controller; 12. 140. (1) add validation in RegisterDto 12.141 (1) user jwt to get user information

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z].{4,8}$)", ErrorMessage = "Password must be complex(1 lower case, 1 upper case, 4-8 characters)")]
         public string Password { get; set; }
-        public string Username { get; set; }
+        [Required]
+        public string UserName { get; set; }
     }
 }
