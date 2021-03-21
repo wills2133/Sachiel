9afb1f015441eaefc81b3a6dcbc1a3b4f3367ce0
wills2133 <wills2133@hotmail.com>
2021-03-21 16:53:32 -0700

    update: 14.163 add Maper to for eager loading to make request success;

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -15,6 +15,6 @@ namespace Application.Activities
         public string City { get; set; }
         public string Category { get; set; }
         public string HostUsername { get; set; }
-        public ICollection<Profile> Profiles { get; set; }
+        public ICollection<Profile> Attendees { get; set; }
     }
 }
