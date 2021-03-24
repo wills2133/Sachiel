ecf1263ba0cb55e828f7065e18de7c85df5794aa
wills2133 <wills2133@hotmail.com>
2021-03-24 15:27:31 -0700

    update: 17.198 add photo adding mode; 17.201 add file drop widget;

---
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropZone.tsx
@@ -0,0 +1,38 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Header, Icon } from 'semantic-ui-react'
+
+interface Props {
+    setFile: (file: any) => void;
+}
+
+export default function PhotoWidgetDropzone({setFile}: Props) {
+
+  const dzStyle = {
+    border: 'dashed 3px #eee',
+    borderColor: '#eee',
+    borderRadius: '5px',
+    paddingTop: '30px',
+    textAlign: 'center' as 'center',
+    height: 200
+  }
+
+  const dzActive = {
+    borderColor: 'green'
+  }
+
+  const onDrop = useCallback(acceptedFiles => {
+    setFile(acceptedFiles.map((file: any) => Object.assign(file, {
+      preview: URL.createObjectURL(file)
+    })))
+  }, [setFile])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()} style={isDragActive ? {...dzStyle, ...dzActive} : dzStyle}>
+      <input {...getInputProps()} />
+      <Icon name='upload' size='huge' />
+      <Header content='Drop file here' />
+    </div>
+  )
+}
--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,6 +1,8 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { Card, Header, Image, Tab, TabPane } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Card, Grid, GridColumn, Header, Image, Tab, TabPane } from 'semantic-ui-react';
+import PhotoUploadWidget from '../../app/common/imageUpload/PhotoUploadWidget';
+import { useStore } from '../../app/stores/store';
 import { Profile } from '../../models/profile';
 
 interface Props {
@@ -8,16 +10,36 @@ interface Props {
 }
 
 export default observer(function ProfilePhotos({profile}: Props) {
+    const {profileStore: {isCurrentUser}} = useStore();
+    const [addPhotoMode, setAddPhotoMode] = useState(false);
+
     return (
         <Tab.Pane>
-            <Header icon='image' content='Photos' />
-            <Card.Group itemsPerRow={5}>
-                {profile.photos?.map((photo) => (
-                    <Card key={photo.id}>
-                        <Image src={photo.url} />
-                    </Card>
-                ))}
-            </Card.Group>
+            <Grid>
+                <GridColumn width={16}>
+                    <Header floated='left' icon='image' content='Photos' />
+                    {isCurrentUser && (
+                        <Button
+                            floated='right'
+                            content={addPhotoMode ? 'Cancle' : 'Add Photo'}
+                            onClick={() => setAddPhotoMode(!addPhotoMode)}
+                        />
+                    )}
+                </GridColumn>
+                <GridColumn width={16}>
+                    {addPhotoMode ? (
+                            <PhotoUploadWidget />
+                        ) : (
+                            <Card.Group itemsPerRow={5}>
+                                {profile.photos?.map((photo) => (
+                                    <Card key={photo.id}>
+                                        <Image src={photo.url} />
+                                    </Card>
+                                ))}
+                            </Card.Group>
+                    )}
+                </GridColumn>
+            </Grid>
         </Tab.Pane>
     )
 })
