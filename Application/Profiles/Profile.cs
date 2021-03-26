ecc6fc4c47f09e49644e58fe19db6d1d785172d9
wills2133 <wills2133@hotmail.com>
2021-03-26 14:02:17 -0700

    update: 20.225 add following count in profile;

--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -9,6 +9,9 @@ namespace Application.Profiles
         public string DisplayName { get; set; }
         public string Bio { get; set; }
         public string Image { get; set; }
+        public bool Following { get; set; }
+        public int FollowingsCount { get; set; }
+        public int FollowersCount { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
 }
