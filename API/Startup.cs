f5b34ce476f3de251d61e6d8bf05865343138e40
wills2133 <wills2133@hotmail.com>
2021-03-28 17:00:07 -0700

    heroku deploy with security policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,13 +58,13 @@ namespace API
             app.UseReferrerPolicy(opt => opt.NoReferrer());
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
-            app.UseCspReportOnly(opt => opt
+            app.UseCsp(opt => opt
                 .BlockAllMixedContent()
                 .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "data:"))
                 .ScriptSources(s => s.Self().CustomSources("sha256-u4qrUfAHW1wvPPkqAaRgAv7FWGmo8g66aTT+e/bdvkY="))
             );
 
