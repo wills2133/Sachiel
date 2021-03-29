d98991fea37ec427f1461806586447484dcccc80
wills2133 <wills2133@hotmail.com>
2021-03-28 17:49:38 -0700

    update: security policy add bob:

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,7 +64,7 @@ namespace API
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "data:", "https://res.cloudinary.com/"))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "data:", "blob:"))
                 .ScriptSources(s => s.Self().CustomSources("sha256-u4qrUfAHW1wvPPkqAaRgAv7FWGmo8g66aTT+e/bdvkY="))
             );
 
